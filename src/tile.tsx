a3b55ee1ac0607e8eefcbd38c56218a4c73c696d
LiamRB-prog <liamrbelh@gmail.com>
2025-07-20 19:50:09 -0500

    First commit

---
+++ src/tile.tsx
@@ -0,0 +1,11 @@
+function Tile() {
+    return (
+        <>
+            <div className="tile">
+                <h1>This is a test</h1>
+            </div>
+        </>
+    )
+}
+
+export default Tile
