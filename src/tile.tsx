087610ebd66dc2af81952b1326eec3b604979c78
Liam Belh <liamrbelh@gmail.com>
2025-07-25 01:43:49 -0500

    Fuck Tailwind

--- src/tile.tsx
+++ src/tile.tsx
@@ -1,9 +1,8 @@
 function Tile() {
     return (
         <>
-            <div className="box-border m-auto h-28 w-32 p-4 
-                border-4 bg-white">
-                This is a test
+            <div className="a">
+                Tailwind is not working!
             </div>
         </>
     )
